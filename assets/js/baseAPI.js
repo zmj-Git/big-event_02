58dfa6b8009325ff0d6c6ea398395850c4798d3a
zmj <1241311306@qq.com>
2021-01-19 21:27:03 +0800

    个人中心功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -18,7 +18,7 @@ $.ajaxPrefilter(function (params) {
     }
     // 3.拦截所有响应，判断身份确认信息
     params.complete = function (res) {
-        console.log(res.responseJSON)
+        // console.log(res.responseJSON)
         var obj = res.responseJSON;
         if (obj.status == 1 && obj.message == '身份认证失败!') {
             localStorage.removeItem("token");
